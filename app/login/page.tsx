85d2bb796c2385a7d99a46684f761c4159bf96e3
Muhammad Umer <mumer119131@gmail.com>
2024-04-28 11:12:39 +0500

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Login() {
+  return (
+    <div>
+        <h1>Login</h1>
+        <form>
+            <label>
+            Username:
+            <input type="text" name="username" />
+            </label>
+            <label>
+            Password:
+            <input type="password" name="password" />
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+  );
+}
